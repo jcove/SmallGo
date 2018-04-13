c0b0b19d552426e5498f93ff4ca8a130d0545111
jcove <jcove@163.com>
2018-04-13 15:33:53 +0800

    修复安装错误问题 增加手机文章模板

--- app/Admin/Extensions/CategoryIcon.php
+++ app/Admin/Extensions/CategoryIcon.php
@@ -17,7 +17,7 @@ class CategoryIcon extends Field\Text
     protected static $css = [
         '/fonticon-picker/css/jquery.fonticonpicker.css',
         '/fonticon-picker/themes/bootstrap-theme/jquery.fonticonpicker.bootstrap.min.css',
-        '/iconfont/iconfont.css'
+        '//at.alicdn.com/t/font_507260_xqcwtl6ymfxyldi.css'
     ];
 
     protected static $js = [
@@ -29,35 +29,35 @@ class CategoryIcon extends Field\Text
         $this->script = <<<EOT
     var icons               =   ["iconfont icon-muyin-qinju","iconfont icon-nanshi-nanxie","iconfont icon-nvshi-peishi","iconfont icon-dianqi-peijian","iconfont icon-nvshi-jiajufu",
                                 "iconfont icon-yinshi-lingshi","iconfont icon-canchu-canju","iconfont icon-yinshi-tiaoweipin","iconfont icon-nvshi-waitao","iconfont icon-canchu-chuju",
-                                "iconfont icon-nvshi-lianyiqun","iconfont icon-dianqi-dianshi","iconfont icon-qita-wenju","iconfont icon-yinshi-baojianpin","iconfont icon-muyin-chechuangyongpin",
+                                "iconfont icon-nvshi-lianyiqun","iconfont icon-dianqi-dianshi","iconfont icon-yinshi-baojianpin","iconfont icon-muyin-chechuangyongpin",
                                 "iconfont icon-nanshi-neiyi","iconfont icon-nvshi-neiyi","iconfont icon-jiaju-jiazhuangzhucai","iconfont icon-yinshi-chongyin","iconfont icon-nanshi-waitao",
-                                "iconfont icon-nvshi-zhenzhishan","iconfont icon-nanshi-xizhuang","iconfont icon-xihu-xifahufa","iconfont icon-jiaju-jujiabuyi","iconfont icon-muyin-yunchan",
-                                "iconfont icon-nanshi-peishi","iconfont icon-nanshi-nanbao","iconfont icon-yinshi-jiulei","iconfont icon-nanshi-jiajufu","iconfont icon-dianqi-bangong",
+                                "iconfont icon-nanshi-xizhuang","iconfont icon-xihu-xifahufa","iconfont icon-jiaju-jujiabuyi","iconfont icon-muyin-yunchan",
+                                "iconfont icon-nanshi-peishi","iconfont icon-nanshi-nanbao","iconfont icon-yinshi-jiulei","iconfont icon-nanshi-jiajufu",
                                 "iconfont icon-nanshi-weiyi","iconfont icon-canchu-qingjiebaoxian","iconfont icon-yundonghuwai-jianshen","iconfont icon-yinshi-shengxian","iconfont icon-yinshi-liangyou",
                                 "iconfont icon-yinshi-shuiguo","iconfont icon-xihu-kouqianghuli","iconfont icon-canchu-daojianzhenban","iconfont icon-canchu-guoju","iconfont icon-dianqi-diannao",
                                 "iconfont icon-yundonghuwai-yundong","iconfont icon-jiaju-qingjie","iconfont icon-nanshi-txu","iconfont icon-nvshi-kuzhuang","iconfont icon-nvshi-weiyi",
                                 "iconfont icon-xihu-hufu","iconfont icon-muyin-yinyouerzhuang","iconfont icon-dianqi-shuma","iconfont icon-muyin-zhiniaoku","iconfont icon-muyin-xihu","iconfont icon-muyin-tongzhuang",
-                                "iconfont icon-nanshi-zhenzhishan","iconfont icon-jiaju-dengshi","iconfont icon-yinshi-shicai","iconfont icon-dianqi-gehujiankang","iconfont icon-qita-qipei",
+                                "iconfont icon-nanshi-zhenzhishan","iconfont icon-jiaju-dengshi","iconfont icon-yinshi-shicai","iconfont icon-dianqi-gehujiankang",
                                 "iconfont icon-dianqi-shouji","iconfont icon-muyin-wanju","iconfont icon-xihu-meizhuang","iconfont icon-jiaju-shouna","iconfont icon-yundonghuwai-huwai","iconfont icon-xihu-shengliyongpin",
-                                "iconfont icon-canchu-beihu","iconfont icon-nvshi-nvbao","iconfont icon-jiaju-weiyu","iconfont icon-qita-zhuoyi","iconfont icon-nvshi-chenshan","iconfont icon-jiaju-jiaju","iconfont icon-jiaju-shipin",
-                                "iconfont icon-muyin-weiyang","iconfont icon-nanshi-chenshan","iconfont icon-qita-chongwu","iconfont icon-nanshi-kuzhuang","iconfont icon-jiaju-chuangpinjiantao","iconfont icon-nanshi-wazi",
-                                "iconfont icon-qita-wujin","iconfont icon-nvshi-nvxie","iconfont icon-nvshi-wazi","iconfont icon-bangong-zhuoyi","iconfont icon-bangong-wenju",'iconfont icon-jingji','iconfont icon-quanchangbaoyou'];
+                                "iconfont icon-canchu-beihu","iconfont icon-nvshi-nvbao","iconfont icon-jiaju-weiyu","iconfont icon-nvshi-chenshan","iconfont icon-jiaju-jiaju","iconfont icon-jiaju-shipin",
+                                "iconfont icon-muyin-weiyang","iconfont icon-nanshi-chenshan","iconfont icon-nanshi-kuzhuang","iconfont icon-jiaju-chuangpinjiantao","iconfont icon-nanshi-wazi","iconfont icon-news1",
+                                "iconfont icon-nvshi-nvxie","iconfont icon-bangong-zhuoyi","iconfont icon-bangong-wenju",'iconfont icon-jingji','iconfont icon-quanchangbaoyou'];
                                 
          var searchicons    =   ["iconfont icon-muyin-qinju","iconfont icon-nanshi-nanxie","iconfont icon-nvshi-peishi","iconfont icon-dianqi-peijian","iconfont icon-nvshi-jiajufu",
                                 "iconfont icon-yinshi-lingshi","iconfont icon-canchu-canju","iconfont icon-yinshi-tiaoweipin","iconfont icon-nvshi-waitao","iconfont icon-canchu-chuju",
-                                "iconfont icon-nvshi-lianyiqun","iconfont icon-dianqi-dianshi","iconfont icon-qita-wenju","iconfont icon-yinshi-baojianpin","iconfont icon-muyin-chechuangyongpin",
+                                "iconfont icon-nvshi-lianyiqun","iconfont icon-dianqi-dianshi","iconfont icon-yinshi-baojianpin","iconfont icon-muyin-chechuangyongpin",
                                 "iconfont icon-nanshi-neiyi","iconfont icon-nvshi-neiyi","iconfont icon-jiaju-jiazhuangzhucai","iconfont icon-yinshi-chongyin","iconfont icon-nanshi-waitao",
-                                "iconfont icon-nvshi-zhenzhishan","iconfont icon-nanshi-xizhuang","iconfont icon-xihu-xifahufa","iconfont icon-jiaju-jujiabuyi","iconfont icon-muyin-yunchan",
-                                "iconfont icon-nanshi-peishi","iconfont icon-nanshi-nanbao","iconfont icon-yinshi-jiulei","iconfont icon-nanshi-jiajufu","iconfont icon-dianqi-bangong",
+                                "iconfont icon-nanshi-xizhuang","iconfont icon-xihu-xifahufa","iconfont icon-jiaju-jujiabuyi","iconfont icon-muyin-yunchan",
+                                "iconfont icon-nanshi-peishi","iconfont icon-nanshi-nanbao","iconfont icon-yinshi-jiulei","iconfont icon-nanshi-jiajufu",
                                 "iconfont icon-nanshi-weiyi","iconfont icon-canchu-qingjiebaoxian","iconfont icon-yundonghuwai-jianshen","iconfont icon-yinshi-shengxian","iconfont icon-yinshi-liangyou",
                                 "iconfont icon-yinshi-shuiguo","iconfont icon-xihu-kouqianghuli","iconfont icon-canchu-daojianzhenban","iconfont icon-canchu-guoju","iconfont icon-dianqi-diannao",
                                 "iconfont icon-yundonghuwai-yundong","iconfont icon-jiaju-qingjie","iconfont icon-nanshi-txu","iconfont icon-nvshi-kuzhuang","iconfont icon-nvshi-weiyi",
                                 "iconfont icon-xihu-hufu","iconfont icon-muyin-yinyouerzhuang","iconfont icon-dianqi-shuma","iconfont icon-muyin-zhiniaoku","iconfont icon-muyin-xihu","iconfont icon-muyin-tongzhuang",
-                                "iconfont icon-nanshi-zhenzhishan","iconfont icon-jiaju-dengshi","iconfont icon-yinshi-shicai","iconfont icon-dianqi-gehujiankang","iconfont icon-qita-qipei",
+                                "iconfont icon-nanshi-zhenzhishan","iconfont icon-jiaju-dengshi","iconfont icon-yinshi-shicai","iconfont icon-dianqi-gehujiankang",
                                 "iconfont icon-dianqi-shouji","iconfont icon-muyin-wanju","iconfont icon-xihu-meizhuang","iconfont icon-jiaju-shouna","iconfont icon-yundonghuwai-huwai","iconfont icon-xihu-shengliyongpin",
-                                "iconfont icon-canchu-beihu","iconfont icon-nvshi-nvbao","iconfont icon-jiaju-weiyu","iconfont icon-qita-zhuoyi","iconfont icon-nvshi-chenshan","iconfont icon-jiaju-jiaju","iconfont icon-jiaju-shipin",
-                                "iconfont icon-muyin-weiyang","iconfont icon-nanshi-chenshan","iconfont icon-qita-chongwu","iconfont icon-nanshi-kuzhuang","iconfont icon-jiaju-chuangpinjiantao","iconfont icon-nanshi-wazi",
-                                "iconfont icon-qita-wujin","iconfont icon-nvshi-nvxie","iconfont icon-nvshi-wazi","iconfont icon-bangong-zhuoyi","iconfont icon-bangong-wenju",'iconfont icon-jingji','iconfont icon-quanchangbaoyou'];
+                                "iconfont icon-canchu-beihu","iconfont icon-nvshi-nvbao","iconfont icon-jiaju-weiyu","iconfont icon-nvshi-chenshan","iconfont icon-jiaju-jiaju","iconfont icon-jiaju-shipin",
+                                "iconfont icon-muyin-weiyang","iconfont icon-nanshi-chenshan","iconfont icon-nanshi-kuzhuang","iconfont icon-jiaju-chuangpinjiantao","iconfont icon-nanshi-wazi","iconfont icon-news1",
+                                "iconfont icon-nvshi-nvxie","iconfont icon-bangong-zhuoyi","iconfont icon-bangong-wenju",'iconfont icon-jingji','iconfont icon-quanchangbaoyou'];
 
 $('#category-icon').fontIconPicker({
         source: icons,
--- resources/views/mobile/default/article/index.blade.php
+++ resources/views/mobile/default/article/index.blade.php
@@ -1,32 +1,45 @@
 @extends('mobile.default.layouts.layout')
 @section('content')
     <div class="article-list">
+        @foreach($list as $item)
+            <li class="">
+                <a href="{{route('article.show',['article'=>$item->id])}}" title="{{$item->title}}">
+                    <div class="item">
+                        <!-- 文章图片-->
+                        <div class="cover" style="background-image: url('{{ $item->cover }}')">
 
-    </div>
-    @foreach($list as $item)
-        <li class="">
-            <a href="{{route('article.show',['article'=>$item->id])}}" title="{{$item->title}}">
-                <div class="item">
-                    <!-- 文章图片-->
-                    <div class="cover">
-                        <img src="{{$item->cover}}" alt="{{$item->title}}">
-                    </div>
-                    <div class="info">
-                        <!-- 文章标题-->
-                        <div class="title">{{$item->title}}</div>
-                        <div class="other">
-                            <div class="pull-right">
-                                <!-- 评论信息-->
-                                <span class="icon-group">
-                                            <i class="zm-icon-comments-o"></i>6                                        </span>
-                                <!-- 收藏信息-->
-                                <span class="icon-group">
-                                            <i class="zm-icon-star-o"></i>20                                        </span>
+                        </div>
+                        <div class="info">
+                            <!-- 文章标题-->
+                            <div class="title">{{$item->title}}</div>
+                            <div class="other">
+                                <div class="">
+                                    <span class="pull-right pub-date">{{$item->created_at}}</span>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </a>
-        </li>
-    @endforeach
+                </a>
+            </li>
+        @endforeach
+    </div>
+
+@endsection
+@section('script')
+    <script>
+        $(function () {
+            //导航
+
+            $(window).scroll(function () {
+                var scrollTop = $(this).scrollTop();
+                var scrollHeight = $(document).height();
+                var windowHeight = $(this).height();
+                if (scrollTop + windowHeight == scrollHeight) {
+                    nextPage(nextPageUrl+'?page='+page,'#result');
+                    page++;
+                }
+            });
+        });
+
+    </script>
 @endsection
